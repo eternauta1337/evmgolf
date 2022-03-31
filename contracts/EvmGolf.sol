ddd100bfbea2f4f7f177814e37d692e90b6308f1
Evelyn Florentine <florentine.evelyn@gmail.com>
2022-03-31 17:04:37 -0400

    added events, updated comments

--- contracts/EvmGolf.sol
+++ contracts/EvmGolf.sol
@@ -1,71 +1,75 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import "./interfaces/ILevel.sol";
 
 contract EvmGolf {
-    mapping(string => address) private _levels; // level name -> level address
-    mapping(address => uint) private _authors; // author -> level address
-    mapping(address => address) private _solutions; // level -> solution
-    mapping(address => address) private _records; // level -> author
+    mapping(string => bool) private _levels; // level name -> bool
+    mapping(address => ILevel) private _authors; // author -> level
+    mapping(ILevel => address) private _solutions; // level -> solution
+    mapping(ILevel => address) private _records; // level -> author
     mapping(address => uint) private _victories; // author -> number of victories
 
-    function registerLevel(address newLevel) external returns (bool) {
-        const levelABI = [ "function name() external returns (string memory)"];
-        const level = new ethers.Contract(newLevel, levelABI, ethers.getDefaultProvider());
-        
+    event LevelSolved(ILevel level, address solution, address player);
+    event LevelFailed(ILevel level, address solution, address player);
+    event LevelRecord(ILevel level, address solution, address player);
+
+    function registerLevel(ILevel newLevel) external {
+        string memory levelName = newLevel.name();
         // Verify that level name is not already registered
-        if (_levels[level.name()]) { return false; }
+        if (_levels[levelName] == true) {
+            // TODO revert
+        }
         
-        // Stores the address of the level in a levels array
-        _levels[level.name()] = newLevel;
+        // Stores the address of the level in a levels set
+        _levels[levelName] = true;
 
         // Stores the address of the author of the level in an authors mapping
         _authors[msg.sender] = newLevel;
 
     }
 
-    function playLevel(address level, address solution) external {
-        const levelABI = [ 
-            "function submit(address candidate) external returns (bool)"
-        ];
-        const levelContract = new ethers.Contract(newLevel, levelABI, ethers.getDefaultProvider());
+    function playLevel(ILevel level, address solution) external {
+        bool success = level.submit(solution);
+        address player = msg.sender;
 
-        // Should this use await? Would submit happen synchronously?
-        const success = levelContract.submit(solution);
+        if (success) {
+            emit LevelSolved(level, solution, player);
+        } else {
+            // TODO revert
+            emit LevelFailed(level, solution, player);
+        }
 
-        // TODO emit events
-        // If the internal call reverts or returns false, emits a LevelFailed(address level, address solution, address player) event.
-        // If the internal call returns true, it emits a LevelSolved(address level, address solution, address player).
-        
-        // TODO how to get byte code from contract to check if success
-        
-        // If success {
-            // If the solution has shorter runtime bytecode than the shortest known solution,
-            
-            // stores it in a solutions mapping and emits a LevelRecord(address level, address solution, address player)
-            past_winner = _solutions[level];
-            winner = msg.sender;
+        bool isRecord = _isRecord(level, solution);
+
+        if (isRecord) {
+            // Stores in solutions mapping
             _solutions[level] = solution;
             // removes the previous record holder from the records array, and adds the new record holder
-            _records[level] = winner;
+            address past_record_holder = _records[level];
+            _records[level] = player;
             // increments and decrements a corresponding victories mapping
-            _decrementVictories(past_winner);
-            _incrementVictories(winner);
-        //}
+            _decrementVictories(past_record_holder);
+            _incrementVictories(player);
+
+            emit LevelRecord(level, solution, player);
+        }
     }
 
-    function _decrementVictories(address player) private view {
+    function _decrementVictories(address player) private {
         // Do not let victories go negative
         if (_victories[player] > 0) {
             _victories[player] -= 1;
         }
     }
 
-    function _incrementVictories(address player) private view {
-        if (_victories[player])  {
-            _victories[player] += 1;
-        } else {
-            _victories[player] = 1;
-        }
+    function _incrementVictories(address player) private {
+        _victories[player] += 1;
+    }
+    
+    function _isRecord(ILevel level, address solution) private view returns (bool) {
+        // TODO figure out how to get bytecode
+        // default to false for now
+        return false;
     }
 
 }
